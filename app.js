431063c8c75f93d0190d761f266214465130b5c5
maisha haider <arakibulislam>
2021-08-21 20:19:49 +0600

    final

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 // ================ For Memory , Storage & Delivery button Function ================ //
 
-function totalCost(product,price) {
+function totalCost(product, price) {
     const cost = document.getElementById(product + '-cost');
     cost.innerText = price;
     totalPrice();
 }
 
-function totalPrice(){
+function totalPrice() {
     const bestPrice = document.getElementById('best-price');
     const memoryPrice = document.getElementById('memory-cost');
     const storagePrice = document.getElementById('storage-cost');
     const deliveryPrice = document.getElementById('delivery-cost');
     const totalPrice = document.getElementById('total-cost');
-    const total = document.getElementById('total'); 
-    totalPrice.innerText = parseInt(bestPrice.innerText)+parseInt(memoryPrice.innerText)+parseInt(storagePrice.innerText)+parseInt(deliveryPrice.innerText);
+    const total = document.getElementById('total');
+    totalPrice.innerText = parseInt(bestPrice.innerText) + parseInt(memoryPrice.innerText) + parseInt(storagePrice.innerText) + parseInt(deliveryPrice.innerText);
     total.innerText = totalPrice.innerText;
 }
 
@@ -34,30 +34,31 @@ document.getElementById('storage-256gb').addEventListener('click', function () {
 })
 
 document.getElementById('storage-512gb').addEventListener('click', function () {
-    totalCost('storage',100);
+    totalCost('storage', 100);
 })
 
 document.getElementById('storage-1tb').addEventListener('click', function () {
-    totalCost('storage',180);
+    totalCost('storage', 180);
 })
 
 // ================ Delivery Button Event ============== //
 
 document.getElementById('delivery-free').addEventListener('click', function () {
-    totalCost( 'delivery',0);
+    totalCost('delivery', 0);
 })
 
 document.getElementById('delivery-pay').addEventListener('click', function () {
-    totalCost( 'delivery',20);
+    totalCost('delivery', 20);
 })
 
 // ================= Promo Button Event ==============//
 
 document.getElementById('apply-promo').addEventListener('click', function () {
     const code = document.getElementById('promo-code')
-    if(code.value == 'stevekaku'){
-        const total = document.getElementById('total');
-    total.innerText = parseInt(total.innerText) -(parseInt(total.innerText)/5);
+    const totalCost = document.getElementById('total-cost');
+    const total = document.getElementById('total');
+    if (code.value == 'stevekaku') {
+        total.innerText = parseInt(totalCost.innerText) - (parseInt(totalCost.innerText) / 5);
     }
     code.value = '';
 })
@@ -65,25 +66,25 @@ document.getElementById('apply-promo').addEventListener('click', function () {
 
 // ================== optional ======================//
 
-function color(images,names){
+function color(images, names) {
     const image = document.getElementById('image');
     const name = document.getElementById('name');
-    image.src = 'images/'+images+'.jpeg';
+    image.src = 'images/' + images + '.jpeg';
     name.innerText = names;
 }
 
-document.getElementById('green').addEventListener('click',function(){
-    color('green','Green')
+document.getElementById('green').addEventListener('click', function () {
+    color('green', 'Green')
 })
-document.getElementById('blue').addEventListener('click',function(){
-    color('blue','Blue')
+document.getElementById('blue').addEventListener('click', function () {
+    color('blue', 'Blue')
 })
-document.getElementById('pink').addEventListener('click',function(){
-    color('pink','Pink')
+document.getElementById('pink').addEventListener('click', function () {
+    color('pink', 'Pink')
 })
-document.getElementById('yellow').addEventListener('click',function(){
-    color('yellow','Yellow')
+document.getElementById('yellow').addEventListener('click', function () {
+    color('yellow', 'Yellow')
 })
-document.getElementById('silver').addEventListener('click',function(){
-    color('silver','Silver')
+document.getElementById('silver').addEventListener('click', function () {
+    color('silver', 'Silver')
 })
